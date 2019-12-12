cc8c35702d7a8e0f6feca42c4ede27801e3b774c
sasadakouhei <c501937014@tokushima-u.ac.jp>
2019-12-12 16:24:11 +0900

    first commit

---
+++ Assets/LeapMotion/Scripts/Function/GetHandPosition.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using System;
+
+public class GetHandPosition : MonoBehaviour
+{
+
+    Controller controller = new Controller();
+    public int FingerList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Frame frame = controller.Frame();
+        HandList hands = frame.Hands;
+        Hand leftHand = null, rightHand = null;
+
+        // 右手、左手を取得
+        if (hands.Count == 1){
+            rightHand = hands[0].IsRight ? hands[0] : null;
+            leftHand = hands[0].IsLeft ? hands[0] : null;
+        }
+        else if (1 < hands.Count & hands.Count < 3){
+            rightHand = hands[0].IsRight ? hands[0] : hands[1];
+            leftHand = hands[0].IsLeft ? hands[0] : hands[1];
+        }
+        else {
+            rightHand = null;
+            leftHand = null;
+        }
+
+        // csv出力処理
+        // 右手のみの場合
+        if (rightHand != null && leftHand == null){
+            outputRightHand(rightHand);
+        }
+        // // 両手の場合
+        else if (rightHand != null && leftHand != null){
+            outputBothHands(rightHand, leftHand);
+        }
+    }
+
+    public void outputRightHand(Hand rightHand)
+    {   
+        foreach (Finger finger in rightHand.Fingers){
+            Debug.Log("id :" + finger.Id +  ", 指 :" + finger.Type());
+        }
+        Debug.Log("____________");
+
+    }
+
+    public void outputBothHands(Hand rightHand, Hand leftHand)
+    {
+        Debug.Log ("両手を認知");
+    }
+}
