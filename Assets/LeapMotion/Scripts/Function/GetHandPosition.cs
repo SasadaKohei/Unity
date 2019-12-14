d919ffb324f552d6eb63972f610a5df2a9870775
sasadakouhei <c501937014@tokushima-u.ac.jp>
2019-12-14 16:59:22 +0900

    change all logic

--- Assets/LeapMotion/Scripts/Function/GetHandPosition.cs
+++ Assets/LeapMotion/Scripts/Function/GetHandPosition.cs
@@ -6,13 +6,24 @@ using System;
 
 public class GetHandPosition : MonoBehaviour
 {
-
     Controller controller = new Controller();
-    public int FingerList;
+    public static Leap.Vector R_thumb = new Leap.Vector();
+    public static Leap.Vector R_index = new Leap.Vector();
+    public static Leap.Vector R_middle = new Leap.Vector();
+    public static Leap.Vector R_ring = new Leap.Vector();
+    public static Leap.Vector R_pinky = new Leap.Vector();
+    public static Leap.Vector L_thumb = new Leap.Vector();
+    public static Leap.Vector L_index = new Leap.Vector();
+    public static Leap.Vector L_middle = new Leap.Vector();
+    public static Leap.Vector L_ring = new Leap.Vector();
+    public static Leap.Vector L_pinky = new Leap.Vector();
+
+    public static int measurementsNum = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -20,44 +31,106 @@ public class GetHandPosition : MonoBehaviour
     {
         Frame frame = controller.Frame();
         HandList hands = frame.Hands;
-        Hand leftHand = null, rightHand = null;
+        Hand leftHand = hands.Leftmost;
+        Hand rightHand = hands.Rightmost;
+        FingerList leftFingers = leftHand.Fingers;
+        FingerList rightFingers = rightHand.Fingers;
+        string identifyHandFlag = "null";
+        int fingerFlag = -1;
 
-        // 右手、左手を取得
-        if (hands.Count == 1){
-            rightHand = hands[0].IsRight ? hands[0] : null;
-            leftHand = hands[0].IsLeft ? hands[0] : null;
-        }
-        else if (1 < hands.Count & hands.Count < 3){
-            rightHand = hands[0].IsRight ? hands[0] : hands[1];
-            leftHand = hands[0].IsLeft ? hands[0] : hands[1];
-        }
-        else {
-            rightHand = null;
-            leftHand = null;
+
+        if(leftHand.Id != -1){
+            identifyHandFlag = "LeftHand";
+            foreach(var left_finger in leftFingers){
+                fingerFlag = left_finger.Id % 10;
+                setFingerPos(identifyHandFlag, left_finger.Id, left_finger.TipPosition);
+            }
         }
 
-        // csv出力処理
-        // 右手のみの場合
-        if (rightHand != null && leftHand == null){
-            outputRightHand(rightHand);
+        if(rightHand.Id != -1){
+            identifyHandFlag = "RightHand";
+            foreach(var right_finger in rightFingers){
+                fingerFlag = right_finger.Id % 10;
+                setFingerPos(identifyHandFlag, right_finger.Id, right_finger.TipPosition);
+            }
         }
-        // // 両手の場合
-        else if (rightHand != null && leftHand != null){
-            outputBothHands(rightHand, leftHand);
+        measurementsNum++;
+        if(measurementsNum < 201){
+            // dataを配列に格納
+            if(leftHand.Id != -1 && leftHand.Id == -1){}
+            else if(leftHand.Id == -1 && leftHand.Id != -1){}
+            else if(leftHand.Id == -1 && leftHand.Id == -1){}
         }
+        else{
+            measurementsNum = 0;
+        }
+        
     }
 
-    public void outputRightHand(Hand rightHand)
-    {   
-        foreach (Finger finger in rightHand.Fingers){
-            Debug.Log("id :" + finger.Id +  ", 指 :" + finger.Type());
+    void setFingerPos(String hand, int flag, Leap.Vector finger){
+        if (hand == "RightHand"){
+            switch(flag)
+            {
+                case 0: 
+                    R_thumb.x = finger.x;
+                    R_thumb.y = finger.y;
+                    R_thumb.z = finger.z;
+                    break;
+                case 1: 
+                    R_index.x = finger.x;
+                    R_index.y = finger.y;
+                    R_index.z = finger.z;
+                    break;
+                case 2:
+                    R_middle.x = finger.x;
+                    R_middle.y = finger.y;
+                    R_middle.z = finger.z;
+                    break;
+                case 3:
+                    R_ring.x = finger.x;
+                    R_ring.y = finger.y;
+                    R_ring.z = finger.z;
+                    break;
+                case 4:
+                    R_pinky.x = finger.x;
+                    R_pinky.y = finger.y;
+                    R_pinky.z = finger.z;
+                    break;
+                default:
+                    break;
+            }
         }
-        Debug.Log("____________");
-
-    }
-
-    public void outputBothHands(Hand rightHand, Hand leftHand)
-    {
-        Debug.Log ("両手を認知");
+        else if(hand == "LeftHand"){
+            switch(flag)
+            {
+                case 0: 
+                    L_thumb.x = finger.x;
+                    L_thumb.y = finger.y;
+                    L_thumb.z = finger.z;
+                    break;
+                case 1: 
+                    L_index.x = finger.x;
+                    L_index.y = finger.y;
+                    L_index.z = finger.z;
+                    break;
+                case 2:
+                    L_middle.x = finger.x;
+                    L_middle.y = finger.y;
+                    L_middle.z = finger.z;
+                    break;
+                case 3:
+                    L_ring.x = finger.x;
+                    L_ring.y = finger.y;
+                    L_ring.z = finger.z;
+                    break;
+                case 4:
+                    L_pinky.x = finger.x;
+                    L_pinky.y = finger.y;
+                    L_pinky.z = finger.z;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
