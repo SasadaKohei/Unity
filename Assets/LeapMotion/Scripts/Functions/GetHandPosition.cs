bc7e2994131261561f364f8769e0c266de0a4aa9
sasadakouhei <c501937014@tokushima-u.ac.jp>
2020-01-29 22:12:59 +0900

    add output right hand data

--- Assets/LeapMotion/Scripts/Functions/GetHandPosition.cs
+++ Assets/LeapMotion/Scripts/Functions/GetHandPosition.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Leap;
 using System;
+using System.Text;
+using System.IO;
 
 public class GetHandPosition : MonoBehaviour
 {
 
     Controller controller = new Controller();
-    public int count = 0;
-    public int rightHandCount = 0;
-    public int leftHandCount = 0;
+    public int count = 0; //計測回数
+    public int rightHandCount = 0;  //右手を書き込んだ瞬間
+    public int leftHandCount = 0;  //左手の書き込んだ瞬間
+    public static int file_num = 0;  //書き込みファイルナンバー
 
     // 座標値保存用
     // 右手
@@ -35,7 +38,27 @@ public class GetHandPosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        file_num = 0;
+        //ファイル書き出し
+        string filePath = string.Format(@"./Assets/Data/data{0}.csv", file_num);
+        StreamWriter sw = new StreamWriter(filePath, false, Encoding.GetEncoding("Shift_JIS"));
+        //ヘッダー出力
+        string[] s1 = { "R_hand : x", "R_hand : y", "R_hand : z", 
+                        "R_thumb : x", "R_thumb : y", "R_thumb : z", 
+                        "R_index : x", "R_index : y", "R_index : zx", 
+                        "R_middle : x", "R_middle : y", "R_middle : z",
+                        "R_ring : x", "R_ring : y", "R_ring : z", 
+                        "R_pinky : x", "R_pinky : y", "R_pinky : z", 
+                        "R_hand_vec : x", "R_hand_vec : y", "R_hand_vec : z",
+                        "R_fin_to_fin :x", "R_fin_to_fin :y", "R_fin_to_fin :z",
+                        "R_dis_thumb", "R_dis_index", "R_dis_middle", "R_dis_ring", "R_dis_pinky"};
+        string s2 = string.Join(",", s1);
+        sw.WriteLine(s2);
+        sw.Close();
     }
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -49,52 +72,61 @@ public class GetHandPosition : MonoBehaviour
         if (hands.Count == 1){
             rightHand = hands[0].IsRight ? hands[0] : null;
             leftHand = hands[0].IsLeft ? hands[0] : null;
+            count++;
         }
         else if (1 < hands.Count & hands.Count < 3){
             rightHand = hands[0].IsRight ? hands[0] : hands[1];
             leftHand = hands[0].IsLeft ? hands[0] : hands[1];
+            count++;
         }
         else {
             rightHand = null;
             leftHand = null;
         }
 
-        // csv出力処理
-        // 右手のみの場合
-        if (rightHand != null && leftHand == null){
-            outputRightHand(rightHand);
-            rightHandCount++;
-        }
-        else if (rightHand == null && leftHand != null){
-            outputLeftHand(leftHand);
-            leftHandCount++;
-        }
-        // // 両手の場合
-        else if (rightHand != null && leftHand != null){
-            outputBothHands(rightHand, leftHand);
-            rightHandCount++;
-            leftHandCount++;
-        }
-
-        count++;
         if ( 1 <= count && count < 201){
-            Debug.Log("計測中");
-        }else if (count <= 201){
+            // csv出力処理
+            // 右手のみの場合
+            if (rightHand != null && leftHand == null){
+                outputRightHand(rightHand);
+                rightHandCount++;
+            }
+            else if (rightHand == null && leftHand != null){
+                outputLeftHand(leftHand);
+                leftHandCount++;
+            }
+            // // 両手の場合
+            else if (rightHand != null && leftHand != null){
+                outputBothHands(rightHand, leftHand);
+                rightHandCount++;
+                leftHandCount++;
+            }
+            // Debug.Log("計測中 file_num :" + file_num + ", count" + count);
+        }else if (201 <= count){
             count = 0;
             rightHandCount = 0;
             leftHandCount = 0;
+            file_num++;
+            Create_csv();
         }
     }
 
-        // 右手のみの検出処理
+
+
+
+
+    // 右手のみの検出処理
     public void outputRightHand(Hand rightHand)
     {
         foreach (Finger finger in rightHand.Fingers){
             identifyHandFlag = "RightHand";
             setFingerPos(identifyHandFlag, finger.Id, finger.TipPosition);
-            Debug.Log("id :" + finger.Id +  ", 右手指 :" + finger.Type());
+            // Debug.Log("id :" + finger.Id +  ", 右手指 :" + finger.Type());
         }
-        Debug.Log("____________");
+        float[] vector_data = new float[29];
+        vector_data = SetData(rightHand.PalmPosition, R_thumb, R_index, R_middle, R_ring, R_pinky, rightHand.PalmNormal);
+        Write_csv(vector_data);
+        // Debug.Log("____________");
 
     }
 
@@ -126,9 +158,9 @@ public class GetHandPosition : MonoBehaviour
     // 座標セット
     public void setFingerPos(String hand, int flag, Leap.Vector finger){
         if (hand == "RightHand"){
-            switch(flag)
+            switch(flag % 10)
             {
-                case 0: 
+                case 0:
                     R_thumb.x = finger.x;
                     R_thumb.y = finger.y;
                     R_thumb.z = finger.z;
@@ -157,7 +189,7 @@ public class GetHandPosition : MonoBehaviour
                     break;
             }
         }else if(hand == "LeftHand"){
-            switch(flag)
+            switch(flag % 10)
             {
                 case 0: 
                     L_thumb.x = finger.x;
@@ -193,15 +225,15 @@ public class GetHandPosition : MonoBehaviour
     //取得したベクトルを全て配列に格納
     float [] SetData(Leap.Vector hand_pos, Leap.Vector thumb, Leap.Vector index, Leap.Vector middle, Leap.Vector ring, Leap.Vector pinky, Leap.Vector vec_hand){
         //毎フレームごとに必要なデータを格納する配列
-        float[] data = new float[25];
+        float[] data = new float[29];
         //手の中心座標
         data[0] = hand_pos.x;
         data[1] = hand_pos.y;
         data[2] = hand_pos.z;
         //親指ベクトル
-        data[3] = thumb.x - hand_pos.x;
-        data[4] = thumb.y - hand_pos.y;
-        data[5] = thumb.z - hand_pos.z;
+        data[3] = thumb.x;
+        data[4] = thumb.y;
+        data[5] = thumb.z;
         //人差し指ベクトル
         data[6] = index.x - hand_pos.x;
         data[7] = index.y - hand_pos.y;
@@ -227,11 +259,63 @@ public class GetHandPosition : MonoBehaviour
         data[22] = middle.y - index.y;
         data[23] = middle.z - index.z;
         // 指先の距離
-        data[24] = (float)Math.Sqrt(System.Math.Pow(data[3]) + System.Math.Pow(data[4]) + System.Math.Pow(data[5]));
-        data[25] = (float)Math.Sqrt(System.Math.Pow(data[6]) + System.Math.Pow(data[7]) + System.Math.Pow(data[8]));
-        data[26] = (float)Math.Sqrt(System.Math.Pow(data[9]) + System.Math.Pow(data[10]) + System.Math.Pow(data[11]));
-        data[27] = (float)Math.Sqrt(System.Math.Pow(data[12]) + System.Math.Pow(data[13]) + System.Math.Pow(data[14]));
-        data[28] = (float)Math.Sqrt(System.Math.Pow(data[15]) + System.Math.Pow(data[16]) + System.Math.Pow(data[17]));
+        data[24] = (float)Math.Sqrt(System.Math.Pow((double)data[3], (double)2) + System.Math.Pow((double)data[4], (double)2) + System.Math.Pow((double)data[5], (double)2));
+        data[25] = (float)Math.Sqrt(System.Math.Pow((double)data[6], (double)2) + System.Math.Pow((double)data[7], (double)2) + System.Math.Pow((double)data[8], (double)2));
+        data[26] = (float)Math.Sqrt(System.Math.Pow((double)data[9], (double)2) + System.Math.Pow((double)data[10], (double)2) + System.Math.Pow((double)data[11], (double)2));
+        data[27] = (float)Math.Sqrt(System.Math.Pow((double)data[12], (double)2) + System.Math.Pow((double)data[13], (double)2) + System.Math.Pow((double)data[14], (double)2));
+        data[28] = (float)Math.Sqrt(System.Math.Pow((double)data[15], (double)2) + System.Math.Pow((double)data[16], (double)2) + System.Math.Pow((double)data[17], (double)2));
         return data;
+    }
+
+    //csvにデータを書き込む
+    public static void Write_csv(float[] data)
+    {
+        try
+        {
+            //受け取ったfloat型配列をstring型に変換
+            string[] data_csv = new string[29];
+            for (int i = 0; i <= 28; ++i)
+            {
+                data_csv[i] = data[i].ToString();
+            }
+
+            //appendをtrueにすると、既存のファイルに追記
+            //appendをfalseにすると、新規ファイルを作成
+            var append = true;
+            //出力用のファイルを開く
+            Debug.Log("書き込み");
+            string filePath = string.Format(@"./Assets/Data/data{0}.csv", file_num);
+            StreamWriter sw = new StreamWriter(filePath, append, Encoding.GetEncoding("Shift_JIS"));
+            //print("%s", % i)
+            //ヘッダー出力
+            string[] s1 = data_csv;
+            string s2 = string.Join(",", s1);
+            sw.WriteLine(s2);
+            sw.Close();
+        //}
+        }
+        catch (System.Exception){
+            Debug.Log("csv書き込みエラー");
+        }
+    }
+
+    //２個目以降のcsvファイルの作成
+    public static void Create_csv(){
+        //csvファイル名の作成
+        string filePath = string.Format(@"./Assets/Data/data{0}.csv", file_num);
+        StreamWriter sw = new StreamWriter(filePath, false, Encoding.GetEncoding("Shift_JIS"));
+        //ヘッダー出力
+        string[] s1 = { "R_hand : x", "R_hand : y", "R_hand : z", 
+                        "R_thumb : x", "R_thumb : y", "R_thumb : z", 
+                        "R_index : x", "R_index : y", "R_index : zx", 
+                        "R_middle : x", "R_middle : y", "R_middle : z",
+                        "R_ring : x", "R_ring : y", "R_ring : z", 
+                        "R_pinky : x", "R_pinky : y", "R_pinky : z", 
+                        "R_hand_vec : x", "R_hand_vec : y", "R_hand_vec : z",
+                        "R_fin_to_fin :x", "R_fin_to_fin :y", "R_fin_to_fin :z",
+                        "R_dis_thumb", "R_dis_index", "R_dis_middle", "R_dis_ring", "R_dis_pinky"};
+        string s2 = string.Join(",", s1);
+        sw.WriteLine(s2);
+        sw.Close();
     }
 }
